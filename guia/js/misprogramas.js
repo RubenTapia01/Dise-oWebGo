7a14a5e08640aec96c6e59367c2c232fc44941e7
ruben_tapia <tapiacorrea181@gmail.com>
2021-11-09 10:07:32 -0500

    Respaldo01

---
+++ guia/js/misprogramas.js
@@ -0,0 +1,11 @@
+
+
+$(".checkbox").click(function(){
+    if($("input.checkbox").is(":checked")){
+        $(".theme").attr("href", "css/dark.css");
+    }else{
+        $(".theme").attr("href", "css/light.css");
+    }
+});
+
+    
